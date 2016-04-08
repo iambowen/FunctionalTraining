6dc439036c4a05a923e9d8326fa2cd94aa03a245
Bowen Ma <iambowen.m@gmail.com>
2016-04-08 20:55:42 +0800

    fix implementation of recursion exercise

--- src/main/scala/com/rea/recursion/RecursionExercises.scala
+++ src/main/scala/com/rea/recursion/RecursionExercises.scala
@@ -4,24 +4,24 @@ package com.rea.recursion
 // Taken from http://tmorris.net/posts/scala-exercises-for-beginners/index.html
 
 /**
- * Ok here are the rules.
- *
- * You can't use any of the standard list functions, like `map`, `filter`, `flatMap`, `append`, `:::`, `:+`, etc.
- * 
- * But you can always use `::` to construct a new list by prepending an element to another list.
- *
- * You CAN and are encouraged to use the solutions from the exercises below to solve the harder
- * ones towards the end.
- *
- * Keep an eye out for repetition and similarities between your answers.
- *
- * REMEMBER: Follow the types, they almost always guide you to the solution.  If it compiles and looks a little
- * too simple, it's probably correct.  As Sherlock Holmes once said, "Each one is suggestive, together they are
- * most certainly conclusive."
- *
- * See if you can make your solution tail recursive, where possible.
- *
- */
+  * Ok here are the rules.
+  *
+  * You can't use any of the standard list functions, like `map`, `filter`, `flatMap`, `append`, `:::`, `:+`, etc.
+  *
+  * But you can always use `::` to construct a new list by prepending an element to another list.
+  *
+  * You CAN and are encouraged to use the solutions from the exercises below to solve the harder
+  * ones towards the end.
+  *
+  * Keep an eye out for repetition and similarities between your answers.
+  *
+  * REMEMBER: Follow the types, they almost always guide you to the solution.  If it compiles and looks a little
+  * too simple, it's probably correct.  As Sherlock Holmes once said, "Each one is suggestive, together they are
+  * most certainly conclusive."
+  *
+  * See if you can make your solution tail recursive, where possible.
+  *
+  */
 object RecursionExercises1 {
 
   def plusOne(n: Int) = n + 1
@@ -30,10 +30,7 @@ object RecursionExercises1 {
 
   // Add two non-negative Integers together.  You are only allowed to use plusOne and minusOne above
   def add(a: Int, b: Int): Int = {
-    if (b == 0)
-      a
-    else
-      add(plusOne(a), minusOne(b))
+    if (b == 0) a else add(plusOne(a), minusOne(b))
   }
 
   // You are not permitted to use any list functions such as map, flatMap, ++, flatten etc
@@ -60,27 +57,26 @@ object RecursionExercises1 {
   // Given a function from A => Boolean, return a list with only those item where the function returned true.
   def filter[A](x: List[A], f: A => Boolean): List[A] = x match {
     case Nil => Nil
-    case head :: tail if f(head) => head :: filter(tail, f)
-    case _ :: tail => filter(tail, f)
+    case head :: tail => if (f(head)) head :: filter(tail, f) else filter(tail, f)
   }
 
   // This pattern should be familiar by now... psst... look at add.
-  def append[A](x: List[A], y: List[A]): List[A] = y match {
-    case Nil => x
-    case head :: tail => append(x ::: List(head), tail)
+  def append[A](x: List[A], y: List[A]): List[A] = x match {
+    case Nil => y
+    case head :: tail => head :: append(tail, y)
   }
 
   // Flatten a list of lists to a single list.  Remember you can't use list.flatten.  Can you use a previous
   // solution to solve this one?
   def flatten[A](x: List[List[A]]): List[A] = x match {
     case Nil => Nil
-    case head :: tail => head ::: flatten(tail)
+    case head :: tail => append(head, flatten(tail))
   }
 
   // Follow the types.  You've done a great job getting here. Follow the types.
   def flatMap[A, B](x: List[A], f: A => List[B]): List[B] = x match {
     case Nil => Nil
-    case head :: tail => f(head) ::: flatMap(tail, f)
+    case head :: tail => append(f(head), flatMap(tail, f))
   }
 
   // Maximum of the empty list is 0
@@ -92,7 +88,7 @@ object RecursionExercises1 {
   // Reverse a list
   def reverse[A](x: List[A]): List[A] = x match {
     case Nil => Nil
-    case head :: tail => reverse(tail) ::: List(head)
+    case head :: tail => append(reverse(tail), List(head))
   }
 
 
